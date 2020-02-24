9769ded71537938078977a6af8a680750fc224ed
erocknine <echeungsk@gmail.com>
2020-02-23 21:41:05 -0500

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,20 @@
+require_relative 'instructor'
+require_relative 'student'
+
 class BoatingTest
+    attr_accessor :student, :boatingTest, :boatingStatus, :instructor
+    @@all = []
+
+    def initialize(student, boatingTest, boatingStatus, instructor)
+        @student = student
+        @boatingTest = boatingTest
+        @boatingStatus = boatingStatus
+        @instructor = instructor
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,35 @@
-class Instructor
+require_relative 'student'
+require_relative 'boatingtest'
 
+class Instructor
+    attr_accessor :name
+    @@all
+
+    def initialize(name)
+        @name = name
+    end
+
+    def pass_student(student, boatingTest)
+        test = BoatiingTest.all.find{|student| student.name == student && student.boatingTest == boatingTest}
+        if test != nil
+            test.boatingStatus = "passed"
+        else
+            test = BoatingTest.new(student, boatingTest, "passed", self)
+        end
+        test
+    end
+
+    def fail_student
+        test = BoatiingTest.all.find{|student| student.name == student && student.boatingTest == boatingTest}
+        if test != nil
+            test.boatingStatus = "failed"
+        else
+            test = BoatingTest.new(student, boatingTest, "failed", self)
+        end
+        test
+    end
+
+    def self.all
+        @@all
+    end
 end
