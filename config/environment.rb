85ce78343baaee61770519b41340749251443359
Ryan Seit <ryan@ryanseit.com>
2020-02-24 17:26:14 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,8 @@
 require 'bundler/setup'
+require_relative '../app/models/student.rb'
+require_relative '../app/models/boatingtest.rb'
+require_relative '../app/models/instructor.rb'
+require 'pry'
+
 Bundler.require
 require_all 'app'
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+# STUDENTS
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+ 
+# INSTRUCTORS
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+ 
+# BOATING TESTS
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "passed", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "passed", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+no_crashing = patrick.add_boating_test("Don't Crash 101", "failed", puff)
+parking = spongebob.add_boating_test("Parallel Parking", "passed", puff)
 
+
+binding.pry
+#leave this here to ensure binding.pry isn't the last line\
