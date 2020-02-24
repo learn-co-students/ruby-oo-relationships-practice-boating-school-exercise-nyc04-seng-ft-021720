27161ff078b8089ff7682b3f368fabe3127ec955
kev-kev <kevin.clark93@gmail.com>
2020-02-24 17:16:09 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -15,10 +15,8 @@ class Instructor
   def pass_student(student, boating_test_name)
     test = BoatingTest.all.find{ |test| test.student == student && test.boating_test_name == boating_test_name}
       if !test
-        puts "false"
         BoatingTest.new(student, boating_test_name, 'passed', self)
       else
-        puts 'true'
         test.boating_test_status = 'passed'
         test
       end
@@ -30,6 +28,7 @@ class Instructor
       BoatingTest.new(student, boating_test_name, 'failed', self)
     else
       test.boating_test_status = 'failed'
+      test
     end
   end
 
